a7bfb50145539289bf6fb8d0287d5382aec3c1b1
Duke <duke@riseup.net>
2012-11-02 13:11:19 -0200

    moved Post class to other file

--- src/com/vertigem/dukex/MainActivity.java
+++ src/com/vertigem/dukex/MainActivity.java
@@ -115,14 +115,4 @@ public class MainActivity extends FragmentActivity {
         }
     	
     }
-    
-    public class Post {
-    	public final String title;
-    	public final String text;
-    	
-    	public Post(String title, String text){
-    		this.title = title;
-    		this.text = text;
-    	}
-     }
 }
---
+++ src/com/vertigem/dukex/Post.java
@@ -0,0 +1,11 @@
+package com.vertigem.dukex;
+
+public class Post {
+	public final String title;
+	public final String text;
+	
+	public Post(String title, String text){
+		this.title = title;
+		this.text = text;
+	}
+ }
